b89ca053bac38359c5f3a75dd68e75b60b74831b
hahmed2-crypto <h.ahmed2@lustudent.com>
2026-07-01 13:52:56 -0700

    Fix shebang, prototype name, and formatting for task 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
-#!/usr/bash/python3
-def print_reversed_list_integers(my_list=[]):
-    for i in range(len(my_list) - 1, -1, -1):
-        print(my_list[i])
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integers(my_list)
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
+
