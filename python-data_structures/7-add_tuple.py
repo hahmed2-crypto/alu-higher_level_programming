e5cf471bc512ea5329d6c2486196e595d6b69d98
hahmed2-crypto <h.ahmed2@lustudent.com>
2026-07-05 03:47:16 -0700

    Fixed PEP bug for 7 and 11 project

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,5 +2,4 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = (tuple_a + (0, 0))[:2]
     b = (tuple_b + (0, 0))[:2]
-# Return a new tuple with the sum of elements
     return (a[0] + b[0], a[1] + b[1])
